95db12baa95dc9cc00a7ffec67519849ae655404
wassimselmi <wassim.selmi@sesame.com.tn>
2025-09-18 19:40:16 +0200

    init proj

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('Plant Project is under maintenance 🛠️');
+});
+
+app.get('/test', (req, res) => {
+  res.send('more plants to come! 🛠️');
+});
+
+app.get('/health', (req, res) => {
+  res.json({ status: 'ok', time: new Date() });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
+});
